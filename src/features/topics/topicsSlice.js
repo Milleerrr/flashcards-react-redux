19d273f7803999b50dcf070bda09faf22234bd2f
ben.miller <ben.miller@siemens.com>
2023-06-05 11:26:50 +0100

    Intial commit

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const topicsSlice = createSlice({
+    name: 'topics',
+    initialState: {
+        topics: []
+    },
+    reducers: {
+        addTopic: (state, action) => {
+
+        }
+    }
+}) 
