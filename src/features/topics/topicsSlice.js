491443ec495d2af0da7cbb23b90d5fceb3601a50
ben.miller <ben.miller@siemens.com>
2023-06-05 11:47:57 +0100

    Updated topicsSlice.js

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -3,11 +3,26 @@ import { createSlice } from "@reduxjs/toolkit";
 const topicsSlice = createSlice({
     name: 'topics',
     initialState: {
-        topics: []
+        topics: {}
     },
     reducers: {
         addTopic: (state, action) => {
-
+            const { id, name, icon} = action.payload;
+            state.topics[id] = {
+                id: id,
+                name: name,
+                icon,
+                quizIds: []
+            };
+        },
+        addQuizIdForTopic: (state, action) => {
+            const { topicId, quizId } = action.payload;
+            state.topics[topicId].quizId.push(quizId);
         }
     }
-}) 
+});
+
+export const selectTopics = (state) => state.topics.topics;
+export const { addTopic, addQuizIdForTopic } = topicsSlice.actions;
+
+export default topicsSlice.reducer;
